caacc59b9f3fc24638a1aff889456ec6e2b08ae2
Ujjawal Kumar Mahawar <90878964+ujjawal2700@users.noreply.github.com>
2024-09-21 02:19:55 +0530

    fixes my databse files linting error

--- src/app/api/database/route.ts
+++ src/app/api/database/route.ts
@@ -1,13 +1,7 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
  
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const petName = searchParams.get('petName');
-  const ownerName = searchParams.get('ownerName');
- 
-  
- 
+export async function GET() {
   const pets = await sql`SELECT name FROM Pets;`;
   return NextResponse.json({ pets }, { status: 200 });
 }
